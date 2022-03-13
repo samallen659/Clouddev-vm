47f6a0ea70344f8dc0b827ef8e3379e26a18454d
Sam Allen <samallen659@gmail.com>
2022-03-13 22:02:22 +0000

    Connected to terraform cloud

--- main.tf
+++ main.tf
@@ -1,9 +1,16 @@
 terraform {
-    required_providers {
-        azurerm = {
-        source  = "hashicorp/azurerm"
-        version = "=2.97.0"
-        }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.97.0"
+    }
+  }
+  cloud {
+    organization = "sama"
+
+    workspaces {
+      name = "Clouddev-vm"
+    }
   }
 }
 
